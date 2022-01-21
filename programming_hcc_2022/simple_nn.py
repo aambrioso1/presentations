7dffe52f2241c29ec5791581a94be76b1257a0e4
aambrioso1 <aambrioso1@gmail.com>
2022-01-21 17:08:20 -0500

    improve look of NN graph

--- programming_hcc_2022/simple_nn.py
+++ programming_hcc_2022/simple_nn.py
@@ -31,45 +31,51 @@ def compute_gradients(X, Y, m, b):
 	for i, x in enumerate(X):
 		y = Y[i]
 		y_hat = m*x + b
-		dm += x * (y - y_hat)
-		db += y - y_hat
+		dm += x * (y - y_hat) # partial with respect to m of cost function
+		db += y - y_hat # partial with respect to b of cost function
 	
 	return [-2*dm/n, -2*db/n]
 
 def optimize(X, Y, num_iterations=10, learning_rate=0.1):
-	m, b = [0,0] #initialize_parameters()
+	m, b = [0,0] # initialize parameters
+	# m, b = initialize_parameters() # Use for random initialization
 	line_list = []
 	
 	for i in range(1, num_iterations+1):
+		# gradients are used to update the parameters (backward propagation)
 		dm, db = compute_gradients(X, Y, m, b)
 		m = m - learning_rate * dm
 		b = b - learning_rate * db
 		graph_count = 5
 
 		# Pick 5 lines to graph as algorithm learns regression line
-		if i % (num_iterations//graph_count) == 0:
+		# Could be improved by pick lines at an exponential rate
+		if i in [1, 10, 100, 1000, 10000, 100000, 100000]:
+		# if i % (num_iterations//graph_count) == 0:
 			line_list.append([m,b])
+			print(f"iterations = {i}")
+		# recompute the cost (forward propagation)
 		cost = compute_cost(X, Y, m, b)
 		# print(f"{cost=}, {m=},{b=},{dm=},{db=}")
 	return line_list
 
-num = 1000
+num = 1000000
 alpha = 0.0001
 learned_line_list = optimize(train_x, train_y, num, alpha)
 
 print(f"The learned line list is {learned_line_list}")
 
 import matplotlib.pyplot as plt
-plt.scatter(train_x, train_y)
+plt.scatter(train_x, train_y, color="red")
 x_list = []
 y_list = []
-for line in learned_line_list:
+for i, line in enumerate(learned_line_list):
 	m, b = line
 	for x in np.arange(0,10,0.1):
 		y = m*x + b
 		x_list.append(x)
 		y_list.append(y)
-	plt.plot(x_list,y_list)
+		plt.plot(x_list,y_list, linewidth=1.0, color="blue")
 plt.show()
 
 
